f9c813109a797ba4162f96832fb42e29a10c1565
Sebastian Zazzi <sebastian@zazzi.se>
2020-10-23 14:45:54 +0200

    worked on jsdocs and more js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 
+
+
+/**
+ * Will generate a random number an print it to DOM.
+ */
 function printRandomNumber() {
     const randomNumber = getRandomNumber();
     addNumberToPage(randomNumber);
 
 }
 
+/**
+ * Generates random number and returns it
+ * @returns {Number} the random number
+ */
 function getRandomNumber() {
     return Math.round(Math.random() * 101);
 }
 
+/**
+ * places the number passed from earlier function along iwith comma and space in to the DOM
+ * @param {Number} number number to place in to DOM
+ */
 function addNumberToPage(number) {
     const div = document.getElementById('result')
     div.append(number + ', ')
     console.log(number)
+
 }
 
-console.log('test')
+
+// temp 
+// /**
+//  * @type {HTMLInputElement}
+//  */
+// const input = document.getElementById('name')
+
+// /**
+//  * @typedef {{ name: String, age: Number}} Person A simple person object
+//  */
