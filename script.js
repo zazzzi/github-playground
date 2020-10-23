18e65d386eb954f3d27080f1344021d512cb353a
Sebastian Zazzi <sebastian@zazzi.se>
2020-10-23 14:05:03 +0200

    added js functions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(test)
+
+function printRandomNumber() {
+    const randomNumber = getRandomNumber();
+    addNumberToPage(randomNumber);
+
+}
+
+function getRandomNumber() {
+    return Math.round(Math.random() * 101);
+}
+
+function addNumberToPage(number) {
+    const div = document.getElementById('result')
+    div.append(number + ', ')
+    console.log(number)
+}
+
+console.log('test')
